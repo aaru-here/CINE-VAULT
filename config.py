72e3bd60db0f1623bbcb6b7883ebebafd4f34249
aaru-here <antusah1983@gmail.com>
2026-06-11 20:33:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,9 +6,9 @@ load_dotenv()
 API_ID = int(getenv("YOUR_API_ID"))
 API_HASH = getenv("YOUR_API_HASH")
 
-BOT_TOKEN = getenv("7846024262:AAHIfNat1VoAELqJ8Vz3ELsl_GPA_HzCDsI")
+BOT_TOKEN = getenv("YOUR_BOT_TOKEN")
 
-MONGO_URI = getenv("mongodb+srv://preetiguptaa067_db_user:preetiguptaa067_db_user@cluster0.36ux9u2.mongodb.net/?appName=Cluster0")
+MONGO_URI = getenv("YOUR_MONGO_URI")
 
 TMDB_API_KEY = getenv("408ea9fa3a07ca9e4b51b390c17c7de8")
 
