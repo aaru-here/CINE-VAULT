46de23540005f95df5955f62f3d859ab8bf185f7
aaru-here <antusah1983@gmail.com>
2026-06-11 16:57:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,13 +3,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("32497835"))
+API_HASH = getenv("a1b8caad29d2426f42a05c2d1b13706a")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = getenv("7846024262:AAHIfNat1VoAELqJ8Vz3ELsl_GPA_HzCDsI")
 
-MONGO_URI = getenv("MONGO_URI")
+MONGO_URI = getenv("mongodb+srv://preetiguptaa067_db_user:preetiguptaa067_db_user@cluster0.36ux9u2.mongodb.net/?appName=Cluster0")
 
-TMDB_API_KEY = getenv("TMDB_API_KEY")
+TMDB_API_KEY = getenv("408ea9fa3a07ca9e4b51b390c17c7de8")
 
-OWNER_ID = int(getenv("OWNER_ID"))
+OWNER_ID = int(getenv("8789995472"))
