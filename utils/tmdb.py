1dce036f1c99841a996ec1fc5f009b076890f04d
aaru-here <antusah1983@gmail.com>
2026-06-11 16:07:18 +0530

    Update tmdb.py

--- utils/tmdb.py
+++ utils/tmdb.py
@@ -1,1 +1,25 @@
-# Phase 1 Part 3
+import aiohttp
+from config import TMDB_API_KEY
+
+BASE_URL = "https://api.themoviedb.org/3"
+
+
+async def search_media(query):
+
+    url = (
+        f"{BASE_URL}/search/multi"
+        f"?api_key={TMDB_API_KEY}"
+        f"&query={query}"
+    )
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+
+            data = await response.json()
+
+            results = data.get("results")
+
+            if not results:
+                return None
+
+            return results[0]
